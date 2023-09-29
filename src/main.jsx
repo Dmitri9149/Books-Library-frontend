e47d02dd39242a0298e4fdf9bfa03bfa9347ef82
Dmitri9149 <dmitri.as.name@gmail.com>
2023-09-29 19:21:22 +0300

    add ApolloProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,37 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import './index.css'
 
+import {
+
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql 
+  } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+const query = gql`
+  query {
+    allAuthors  {
+      name,
+      born,
+      bookCount,
+      id
+    }
+  }
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>
 )
