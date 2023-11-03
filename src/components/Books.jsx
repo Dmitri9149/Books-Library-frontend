9b517bf849de491500b6dbff43a0a1418f3f2c95
Dmitri9149 <dmitri.as.name@gmail.com>
2023-11-03 16:25:42 +0200

    reset cache by cliking the 'genre' button to see newly added book

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,13 +1,42 @@
 import { useState } from 'react'
+import { useQuery, useApolloClient } from '@apollo/client'
+import { BOOKS_IN_GENRE } from '../queries'
 const Books = ({books}) => {
-  console.log("BOOKS", books)
-  const [genre, setGenre] = useState('all genres')
+  const client = useApolloClient()
+  console.log("BOOKS imported", books)
+  const [genre, setGenre] = useState("all genres")
   const getGenres = books.map(b => b.genres).flat()
 // from https://stackoverflow.com/questions/1960473/get-all-unique-values-in-a-javascript-array-remove-duplicates
   const allGenres = [...new Set(getGenres)].concat(['all genres']) 
-  const booksToShow = books.filter(book => genre === 'all genres' ?book :book.genres.some(g => g === genre))
-  console.log("allGenres!!!!!!!!!", allGenres, getGenres)
+//  const booksToShow = books.filter(book => genre === 'all genres' ?book :book.genres.some(g => g === genre))
+ console.log("allGenres!!!!!!!!!", allGenres, getGenres)
   console.log("Genre", genre)
+
+  const resultBooks = useQuery(BOOKS_IN_GENRE, 
+    {variables: {genre}})
+
+
+  if (resultBooks.loading) {
+    return <div>loading...</div>
+  }
+
+  console.log("Books.data in Books", resultBooks.data)
+
+  const flattenBooks = resultBooks.data.allBooks.map(b => 
+    { const { title, published, author, genres } = b   
+      const bookFlatten = { 
+        title, 
+        published, 
+        bookCount: author.bookCount,
+        authorName: author.name,
+        authorBorn: author.born,
+        genres
+      }
+      return bookFlatten
+    } )
+
+//    const getGenres = flattenBooks.map(b => b.genres).flat()
+//    const allGenres = [...new Set(getGenres)].concat(['all genres']) 
 
   return (
     <div>
@@ -21,7 +50,7 @@ const Books = ({books}) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {booksToShow.map((b) => (
+          {flattenBooks.map((b) => (
             <tr key={b.title}>
               <td>{b.title}</td>
               <td>{b.authorName}</td>
@@ -32,7 +61,7 @@ const Books = ({books}) => {
       </table>
       <div>
         {allGenres.map((genre) => (
-          <button onClick={() => setGenre(genre)}>{genre}</button>
+          <button onClick={() => {client.resetStore(); setGenre(genre)} }>{genre}</button>
         ))}
       </div>
     </div>
